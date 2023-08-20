a98c188beb2aac4ee5991685ab95b30362871a63
Ivan <dirtyhornet277@gmail.com>
2023-08-20 21:36:47 +0300

    Revert "🌟 Добавлен хэндлер на получение данных из конфига"
    This reverts commit f51140c41ad623c1f3d61d799cbe36ca89c25ec6.

--- app.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, Dispatcher
-from core.handlers.startup import startup_bot
-import asyncio
-
-async def start():
-    bot = Bot(token="")
-    dp = Dispatcher()
-
-    dp.startup.register(startup_bot)
--- core/handlers/startup.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Bot
-from core.config import get_config
-
-async def startup_bot(bot: Bot):
-    await bot.send_message(get_config(".env").ADMIN_ID, "<b>БОТ</b> запустился и готов к работе!")
