f93192a2ec9c6df2563f86f33fa6207113aa2021
Ivan <dirtyhornet277@gmail.com>
2023-08-21 14:18:45 +0300

    🌟 Добавлено логгирование

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from core.handlers.startup import startup_bot
 from core.config import get_config
+import logging
 import asyncio
 
 async def start():
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     bot = Bot(token=get_config(".env").BOT_TOKEN, parse_mode=ParseMode.MARKDOWN_V2)
     dp = Dispatcher()
 
--- core/handlers/startup.py
+++ core/handlers/startup.py
@@ -2,4 +2,4 @@ from aiogram import Bot
 from core.config import get_config
 
 async def startup_bot(bot: Bot):
-    await bot.send_message(get_config(".env").ADMIN_ID, "*БОТ* запустился и готов к работе!")
+    await bot.send_message(get_config(".env").ADMIN_ID, "*БОТ* запустился и готов к работе\!")
