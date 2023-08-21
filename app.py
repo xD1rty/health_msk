5e367eeae642a7a7184a3c57ed1b8f28290ba767
Ivan <dirtyhornet277@gmail.com>
2023-08-21 14:16:31 +0300

    🌟 Добавлен хэндлер на отправку сообщения на отключения бота

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
 from core.handlers.startup import startup_bot
 from core.config import get_config
 import asyncio
 
 async def start():
-    bot = Bot(token=get_config(".env").BOT_TOKEN)
+    bot = Bot(token=get_config(".env").BOT_TOKEN, parse_mode=ParseMode.MARKDOWN_V2)
     dp = Dispatcher()
 
     dp.startup.register(startup_bot)
+    dp.shutdown.register(startup_bot)
 
     try:
         await dp.start_polling(bot)
---
+++ core/handlers/shutdown.py
@@ -0,0 +1,5 @@
+from aiogram import Bot
+from core.config import get_config
+
+async def startup_bot(bot: Bot):
+    await bot.send_message(get_config(".env").ADMIN_ID, "*БОТ* выключился")
